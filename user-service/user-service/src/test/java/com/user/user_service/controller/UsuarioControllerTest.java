40ec91b92be37bed0818e703426d0e0a3f38bc2f
LucasCrespo75 <lucascrespo_rodrigues@live.com>
2025-03-24 18:26:06 -0300

    FIX: ajuste do gateway de acordo e orquestração de ms

--- user-service/user-service/src/test/java/com/user/user_service/controller/UsuarioControllerTest.java
+++ user-service/user-service/src/test/java/com/user/user_service/controller/UsuarioControllerTest.java
@@ -48,26 +48,9 @@ class UsuarioControllerTest {
         when(usuarioService.listar()).thenReturn(List.of(usuarioDTO));
     }
 
-    /*
-    @Test
-    void deveSalvarUsuarioComSucesso() throws Exception {
-        UsuarioDTO novoUsuario = new UsuarioDTO(null, "Novo Usuario3", "novo3@email.com", null);
-
-        when(usuarioService.salvar(novoUsuario)).thenReturn(novoUsuario);
-
-        mockMvc.perform(post("/usuarios")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(novoUsuario)))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.nome").value("Novo Usuario3"))
-                .andExpect(jsonPath("$.email").value("novo3@email.com"));
-    }
-
-     */
-
     @Test
     void deveBuscarUsuarioPorIdComSucesso() throws Exception {
-        mockMvc.perform(get("/usuarios/5"))
+        mockMvc.perform(get("/usuarios/7"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.nome").value("Emanoel Silva"));
     }
@@ -84,7 +67,7 @@ class UsuarioControllerTest {
 
         when(usuarioService.salvar(usuarioEditado)).thenReturn(usuarioEditado);
 
-        mockMvc.perform(put("/usuarios/5")
+        mockMvc.perform(put("/usuarios/7")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(usuarioEditado)))
                 .andExpect(status().isOk())
