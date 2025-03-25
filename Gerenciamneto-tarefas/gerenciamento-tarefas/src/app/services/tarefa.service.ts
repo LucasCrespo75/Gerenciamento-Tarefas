de6743830460353cd4215adacf190e3ad2b30777
LucasCrespo75 <lucascrespo_rodrigues@live.com>
2025-03-25 12:28:38 -0300

    fix: ajustes e limpeza no front

--- Gerenciamneto-tarefas/gerenciamento-tarefas/src/app/services/tarefa.service.ts
+++ Gerenciamneto-tarefas/gerenciamento-tarefas/src/app/services/tarefa.service.ts
@@ -6,40 +6,31 @@ import { Observable, Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class TarefaService {
-  getTarefas() {
-    throw new Error('Method not implemented.');
-  }
-  
-  private apiUrl = '/api/tarefas';  // Agora apontando para o API Gateway
+  private apiUrl = 'http://localhost:8085/tarefas';  
 
-  private tarefaEditadaSubject = new Subject<any>(); 
+  private tarefaEditadaSubject = new Subject<any>();
   tarefaEditada$ = this.tarefaEditadaSubject.asObservable();
 
-  constructor(private http: HttpClient) {}
-
- 
+  constructor(private http: HttpClient) { }
 
   buscarTarefas(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl); 
+    return this.http.get<any[]>(this.apiUrl);  
   }
 
-
   getTarefaById(id: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/${id}`);
+    return this.http.get(`${this.apiUrl}/${id}`); 
   }
 
   criarTarefa(tarefa: any): Observable<any> {
-    return this.http.post(this.apiUrl, tarefa);
+    return this.http.post(this.apiUrl, tarefa);  
   }
 
   editarTarefa(id: number, tarefa: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/${id}`, tarefa);
-  }
-  emitirTarefaEditada(tarefa: any) {
-    this.tarefaEditadaSubject.next(tarefa);
+    return this.http.put<any>(`${this.apiUrl}/${id}`, tarefa);  
   }
 
+
   excluirTarefa(id: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${id}`);
+    return this.http.delete(`${this.apiUrl}/${id}`);  
   }
 }
